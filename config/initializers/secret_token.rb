d1736eddecd302183a1df5a3f26e6557e11fa0ea
Truong Hoang Dung <revskill09@gmail.com>
2013-12-02 14:18:47 +0700

    First init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qlgd::Application.config.secret_token = '3e8eb78a3fa804a07ee5061120403180e98ee6de318d0bd7bca5ff9fd81ed61f41cf76622c59b94de16fe66cbfb6f6ce2d8c6bf811cc136ce0d679942e1ae958'
