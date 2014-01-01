f67e729ec2836b1d59923dd7d88a273960a74046
Revskill <checkraiser11@gmail.com>
2014-01-01 08:56:37 +0700

    Update order

--- app/controllers/lop_mon_hocs_controller.rb
+++ app/controllers/lop_mon_hocs_controller.rb
@@ -2,31 +2,27 @@
 require 'lop_assignment_group_serializer'
 class LopMonHocsController < ApplicationController
 	def show
-		
-			@lop = LopMonHoc.find(params[:id])
-			authorize @lop, :update?
-			enrollments = @lop.enrollments    
-	    	results = enrollments.map {|e| LopEnrollmentSerializer.new(e)}
-	    	render json: {:lop => LopMonHocSerializer.new(@lop), :enrollments => results}
-    	
+		@lop = LopMonHoc.find(params[:id])
+		authorize @lop, :update?
+		enrollments = @lop.enrollments    
+    	results = enrollments.map {|e| LopEnrollmentSerializer.new(e)}
+    	render json: {:lop => LopMonHocSerializer.new(@lop), :enrollments => results}
 	end
 	def info
 		@lop = LopMonHoc.find(params[:lop_id])
 		render json: LopMonHocSerializer.new(@lop)
 	end
 	def update
-		
-			@lop = LopMonHoc.find(params[:id])	
-			@lop.settings ||= {}	
-			@lop.settings["so_tiet_ly_thuyet"] = params[:lt].to_i
-	    	@lop.settings["so_tiet_thuc_hanh"] = params[:th].to_i
-	    	@lop.settings["language"] = params[:lang]
-	    	@lop.settings["de_cuong_du_kien"] = params[:decuong]
-	    	@lop.start!
-			enrollments = @lop.enrollments    
-	    	results = enrollments.map {|e| LopEnrollmentSerializer.new(e)}
-	    	render json: {:lop => LopMonHocSerializer.new(@lop), :enrollments => results}
-	    
+		@lop = LopMonHoc.find(params[:id])	
+		@lop.settings ||= {}	
+		@lop.settings["so_tiet_ly_thuyet"] = params[:lt].to_i
+    	@lop.settings["so_tiet_thuc_hanh"] = params[:th].to_i
+    	@lop.settings["language"] = params[:lang]
+    	@lop.settings["de_cuong_du_kien"] = params[:decuong]
+    	@lop.start!
+		enrollments = @lop.enrollments    
+    	results = enrollments.map {|e| LopEnrollmentSerializer.new(e)}
+    	render json: {:lop => LopMonHocSerializer.new(@lop), :enrollments => results}
 	end
 	
 	def reorder_assignment_group
@@ -46,26 +42,23 @@ class LopMonHocsController < ApplicationController
 	
 	# delete assignment group
 	def delete_assignment_group
-		
-			@lop = LopMonHoc.find(params[:id])
-			@as = @lop.assignment_groups.find(params[:assignment_group_id]);
-			@as.destroy if @as
-			results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
-			render json: results.to_json
-		
+		@lop = LopMonHoc.find(params[:id])
+		@as = @lop.assignment_groups.find(params[:assignment_group_id]);
+		@as.destroy if @as
+		results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
+		render json: results.to_json
 	end
 	# update assignment group
 	def update_assignment_group
-		
-			@lop = LopMonHoc.find(params[:id])
-			@as = @lop.assignment_groups.find(params[:assignment_group_id]);
-			if @as
-				@as.name = params[:name]
-				@as.weight = params[:weight]
-				@as.save!
-			end
-			results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
-			render json: results.to_json
+		@lop = LopMonHoc.find(params[:id])
+		@as = @lop.assignment_groups.find(params[:assignment_group_id]);
+		if @as
+			@as.name = params[:name]
+			@as.weight = params[:weight]
+			@as.save!
+		end
+		results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
+		render json: results.to_json
 	end
 	# get assignments
 	def assignments		
@@ -79,7 +72,6 @@ class LopMonHocsController < ApplicationController
 		@assignment.move_to_bottom
 		results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
 		render json: results.to_json
-		
 	end
 	def reorder_assignment
 		@lop = LopMonHoc.find(params[:id])
@@ -90,77 +82,69 @@ class LopMonHocsController < ApplicationController
 	end
 	# update assignment
 	def update_assignment
-		
-			@lop = LopMonHoc.find(params[:id])
-			@a = @lop.assignments.find(params[:assignment_id]);
-			if @a
-				@a.name = params[:name]
-				@a.points = params[:points]
-				@a.save!
-			end
-			results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
-			render json: results.to_json
-		
+		@lop = LopMonHoc.find(params[:id])
+		@a = @lop.assignments.find(params[:assignment_id]);
+		if @a
+			@a.name = params[:name]
+			@a.points = params[:points]
+			@a.save!
+		end
+		results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
+		render json: results.to_json
 	end
 	# delete assignment
 	def delete_assignment
-		
-			@lop = LopMonHoc.find(params[:id])
-			@a = @lop.assignments.find(params[:assignment_id]);
-			@a.destroy if @a
-			results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
-			render json: results.to_json
-		
+		@lop = LopMonHoc.find(params[:id])
+		@a = @lop.assignments.find(params[:assignment_id]);
+		@a.destroy if @a
+		results = @lop.assignment_groups.map {|g| g and LopMonHocAssignmentGroupSerializer.new(g)}
+		render json: results.to_json
 	end
 
 	# get grades
 	def submissions
-		
-			@lop = LopMonHoc.find(params[:id])
-			assignments = @lop.assignment_groups.includes(:assignments).inject([]) {|res, el| res + el.assignments}
-			count = 0
-			names = [{:name => "Họ và tên"}]
-			names += assignments.map {|a| {:name => a.name, :points => a.points, :group_name => a.assignment_group.name, :group_weight => a.assignment_group.weight}}
-			enrollments = @lop.enrollments
-			results = enrollments.map do |en|
-				tmp = {:name => en.sinh_vien.hovaten, :assignments => []}			
-				assignments.each_with_index do |as, index|				
-					tmp[:assignments] << EnrollmentSubmissionSerializer.new(EnrollmentSubmissionDecorator.new(en, as, index + count))
-				end
-				count += assignments.count
-				tmp
+		@lop = LopMonHoc.find(params[:id])
+		assignments = @lop.assignment_groups.includes(:assignments).inject([]) {|res, el| res + el.assignments}
+		count = 0
+		names = [{:name => "Họ và tên"}]
+		names += assignments.map {|a| {:name => a.name, :points => a.points, :group_name => a.assignment_group.name, :group_weight => a.assignment_group.weight}}
+		enrollments = @lop.enrollments
+		results = enrollments.map do |en|
+			tmp = {:name => en.sinh_vien.hovaten, :assignments => []}			
+			assignments.each_with_index do |as, index|				
+				tmp[:assignments] << EnrollmentSubmissionSerializer.new(EnrollmentSubmissionDecorator.new(en, as, index + count))
 			end
-		    render json: {:names => names, :results => results}.to_json		
-		
+			count += assignments.count
+			tmp
+		end
+	    render json: {:names => names, :results => results}.to_json		
 	end
 	# post grades
 	def update_submissions
-		
-			@lop = LopMonHoc.find(params[:id])
-			@as= @lop.assignments.find(params[:assignment_id])
-			@sub = @as.submissions.where(sinh_vien_id: params[:sinh_vien_id]).first
-			if @sub
-				@sub.grade = params[:grade]
-				@sub.giang_vien_id = params[:giang_vien_id]
-				@sub.save!
-			else
-				@sub = @as.submissions.create(sinh_vien_id: params[:sinh_vien_id], giang_vien_id: params[:giang_vien_id], grade: params[:grade])
-			end
+		@lop = LopMonHoc.find(params[:id])
+		@as= @lop.assignments.find(params[:assignment_id])
+		@sub = @as.submissions.where(sinh_vien_id: params[:sinh_vien_id]).first
+		if @sub
+			@sub.grade = params[:grade]
+			@sub.giang_vien_id = params[:giang_vien_id]
+			@sub.save!
+		else
+			@sub = @as.submissions.create(sinh_vien_id: params[:sinh_vien_id], giang_vien_id: params[:giang_vien_id], grade: params[:grade])
+		end
 
-			assignments = @lop.assignments
-			count = 0
-			names = [{:name => "Họ và tên"}]
-			names += assignments.map {|a| {:name => a.name, :points => a.points, :group_name => a.assignment_group.name, :group_weight => a.assignment_group.weight}}
-			enrollments = @lop.enrollments
-			results = enrollments.map do |en|
-				tmp = {:name => en.sinh_vien.hovaten, :assignments => []}			
-				assignments.each_with_index do |as, index|				
-					tmp[:assignments] << EnrollmentSubmissionSerializer.new(EnrollmentSubmissionDecorator.new(en, as, index + count))
-				end
-				count += assignments.count
-				tmp
+		assignments = @lop.assignments
+		count = 0
+		names = [{:name => "Họ và tên"}]
+		names += assignments.map {|a| {:name => a.name, :points => a.points, :group_name => a.assignment_group.name, :group_weight => a.assignment_group.weight}}
+		enrollments = @lop.enrollments
+		results = enrollments.map do |en|
+			tmp = {:name => en.sinh_vien.hovaten, :assignments => []}			
+			assignments.each_with_index do |as, index|				
+				tmp[:assignments] << EnrollmentSubmissionSerializer.new(EnrollmentSubmissionDecorator.new(en, as, index + count))
 			end
-		    render json: {:names => names, :results => results}.to_json	
-	    
+			count += assignments.count
+			tmp
+		end
+	    render json: {:names => names, :results => results}.to_json	
 	end
 end
