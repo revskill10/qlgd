f55b62846ebeb8348d88b4523d45933730014bf0
Truong Hoang Dung <revskill09@gmail.com>
2014-01-13 13:06:40 +0700

    Update lop policy

--- app/policies/lop_mon_hoc_policy.rb
+++ app/policies/lop_mon_hoc_policy.rb
@@ -12,7 +12,7 @@ LopMonHocPolicy  = Struct.new(:user, :lop_mon_hoc) do
 
     
   def update?
-    return true if user.decorate.is_super_admin?
+    return true if user and user.decorate.is_super_admin?
     user and user.get_lops.map(&:id).include?(lop_mon_hoc.id) and !lop_mon_hoc.completed? and !lop_mon_hoc.removed? and !user.giang_vien(lop_mon_hoc).nil?
   end
 end
