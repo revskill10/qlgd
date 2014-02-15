69b5014d0ecbcc308bbbb4d4b931bbb4ae604959
Truong Hoang Dung <revskill09@gmail.com>
2014-02-15 10:04:18 +0700

    Fix ten giang vien trong lich dang ky

--- app/serializers/daotao/lich_trinh_giang_day_serializer.rb
+++ app/serializers/daotao/lich_trinh_giang_day_serializer.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class Daotao::LichTrinhGiangDaySerializer < ActiveModel::Serializer
   self.root = false
-  attributes :id, :color_status, :tiet_bat_dau, :type_status, :alias_state, :color, :alias_status, :tuan, :thoi_gian, :phong, :status, :so_tiet, :ma_lop, :ten_mon_hoc, :thu, :ltype
+  attributes :id, :color_status, :tiet_bat_dau, :type_status, :alias_state, :color, :alias_status, :tuan, :thoi_gian, :phong, :status, :so_tiet, :ma_lop, :ten_mon_hoc, :thu, :ltype, :giang_vien
   DAYS = {0 => 'Chủ nhật', 1 => 'Thứ hai', 2 => 'Thứ ba', 3 => 'Thứ tư', 4 => 'Thứ năm', 5 => 'Thứ sáu', 6 => 'Thứ bảy'}
   def thoi_gian
     object.thoi_gian.localtime.strftime("%Hh%M %d/%m/%Y")
@@ -9,6 +9,9 @@ class Daotao::LichTrinhGiangDaySerializer < ActiveModel::Serializer
   def thu
     DAYS[object.thoi_gian.localtime.wday]
   end    
+  def giang_vien
+    object.giang_vien.hovaten
+  end
   def color_status
     object.color_status
   end
